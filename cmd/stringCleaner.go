af5f1c98e6dc1c81e5db0a37fb2dc6c90377f96f
connor <connor.alexander@my.uwrf.edu>
2023-11-18 09:23:31 -0600

    create string cleaner function

---
+++ cmd/stringCleaner.go
@@ -0,0 +1,14 @@
+package cmd
+
+import (
+	"strings"
+)
+
+func CleanString(str string) string {
+	// Replace all newline and tab characters with a space
+	str = strings.ReplaceAll(str, "\n", " ")
+	str = strings.ReplaceAll(str, "\t", " ")
+
+	// Trim any leading and trailing white spaces
+	return strings.TrimSpace(str)
+}
