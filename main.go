5fe9f4e4f3e2b67009162f60f31f0b1f3efc8187
connor <connor.alexander@my.uwrf.edu>
2023-11-18 08:22:57 -0600

    remove comments

--- main.go
+++ main.go
@@ -20,12 +20,10 @@ func main() {
 	c.OnHTML("a.colorbox[href]", func(e *colly.HTMLElement) {
 		originalLink := e.Attr("href")
 
-		// Split the URL at '&' to get individual parameters
 		params := strings.Split(originalLink, "&")
 		cleanedParams := make([]string, len(params))
 
 		for i, param := range params {
-			// For each parameter, split by '=' and trim spaces
 			parts := strings.Split(param, "=")
 			if len(parts) == 2 {
 				parts[1] = strings.TrimSpace(parts[1])
@@ -33,7 +31,6 @@ func main() {
 			cleanedParams[i] = strings.Join(parts, "=")
 		}
 
-		// Reconstruct the URL
 		cleanedLink := strings.Join(cleanedParams, "&")
 
 		if strings.Contains(cleanedLink, "courseLightbox.cfm?subject=CIDS") {
@@ -45,16 +42,14 @@ func main() {
 	c.OnHTML("div#classSchedule", func(e *colly.HTMLElement) {
 		var classDetails models.ClassDetails
 
-		// Extracting Subject, Catalog Number, and Title
 		e.ForEach("table tr.tr-background", func(i int, el *colly.HTMLElement) {
-			if i == 1 { // Second row contains the details
+			if i == 1 {
 				classDetails.Subject = el.ChildText("td:nth-child(1)")
-				classDetails.CatalogNum = strings.TrimSpace(el.ChildText("td:nth-child(2)")) // Remove any spaces
+				classDetails.CatalogNum = strings.TrimSpace(el.ChildText("td:nth-child(2)"))
 				classDetails.Title = el.ChildText("td:nth-child(3)")
 			}
 		})
 
-		// Extracting Description
 		classDetails.Description = e.ChildText("table tr:nth-child(3) td[colspan='3']")
 
 		classes = append(classes, classDetails)
@@ -70,7 +65,6 @@ func main() {
 
 	c.Visit("https://www.uwrf.edu/ClassSchedule/DepartmentCourses.cfm?subject=CIDS")
 
-	// Ensure all requests are finished
 	c.Wait()
 
 	jsonData, err := json.MarshalIndent(classes, "", "    ")
